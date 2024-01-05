6a5c5fb51d3328dfcffeba2b5dbfca19d823a623
Dimas Septyanto <gn.dimas@gmail.com>
2024-01-05 15:59:19 +0700

    Start building navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,7 +7,11 @@ import {signIn, signOut, useSession, getProviders} from 'next-auth/react';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className='flex between w-full mb-16 pt-3'>
+      <Link href={'/'} className='flex gap-2 flex-center'>
+        <Image src={'/assets/images/logo.svg'} alt='Promptopia Image'/>
+      </Link>
+    </nav>
   )
 }
 
