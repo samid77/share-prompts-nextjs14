ccd1176603a22e55244566e41657532508579509
Dimas Septyanto <gn.dimas@gmail.com>
2024-01-05 16:00:14 +0700

    Start building navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,7 +9,13 @@ const Nav = () => {
   return (
     <nav className='flex between w-full mb-16 pt-3'>
       <Link href={'/'} className='flex gap-2 flex-center'>
-        <Image src={'/assets/images/logo.svg'} alt='Promptopia Image'/>
+        <Image 
+          src={'/assets/images/logo.svg'} 
+          alt='Promptopia Image'
+          width={30}
+          height={30}
+          className='object-contain'
+          />
       </Link>
     </nav>
   )
