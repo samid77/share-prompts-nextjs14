3b973f4b4e7116dfc0bda32992cae4ed1d698646
Dimas Septyanto <gn.dimas@gmail.com>
2024-01-08 11:00:14 +0700

    Setup models for User and establish connection to MongoDB

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-import User from '@models/user';
+import User from '@models/User';
 import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
@@ -13,16 +13,15 @@ const handler = NextAuth({
   ],
   callbacks: {
     async session({ session }) {
-      // store the user id from MongoDB to session
-      const sessionUser = await User.findOne({ email: session.user.email });
-      session.user.id = sessionUser._id.toString();
+      // // store the user id from MongoDB to session
+      // const sessionUser = await User.findOne({ email: session.user.email });
+      // session.user.id = sessionUser._id.toString();
 
-      return session;
+      // return session;
     },
     async signIn({ account, profile, user, credentials }) {
       try {
         await connectToDB();
-
         // check if user already exists
         const userExists = await User.findOne({ email: profile.email });
 
