c717153e2206dcd0138ac4aafc95ccd30c8e613a
Dimas Septyanto <gn.dimas@gmail.com>
2024-01-08 14:33:06 +0700

    Create post page

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -40,7 +40,7 @@ const Nav = () => {
           ? (
             <div className='flex gap-3 md:gap-5'>
               <Link href={'/create-prompt'} className='black_btn'>Create Post</Link>
-              <button type='button' onClick={() => signOut} className='outline_btn'>Sign Out</button>
+              <button type='button' onClick={() => signOut()} className='outline_btn'>Sign Out</button>
               <Link href={'/profile'}>
                 <Image 
                   src={`${session?.user.image}`}
