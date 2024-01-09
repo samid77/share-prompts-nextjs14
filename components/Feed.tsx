67bb32bf30b2621cf569b3b197293d5212316193
Dimas Septyanto <gn.dimas@gmail.com>
2024-01-09 16:23:48 +0700

    Config API route to fetch prompt data

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -4,12 +4,22 @@ import PromptCard from './PromptCard';
 import PromptCardList from './PromptCardList';
 
 const Feed = () => {
-
   const [searchText, setSearchText] = useState('');
-
+  const [posts, setPosts] = useState([]);
   const handleSearchChange = (e:React.ChangeEvent<HTMLInputElement>) => {
 
   }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const resp = await fetch(`/api/prompt`);
+      const data = await resp.json();
+
+      setPosts(data);
+    }
+
+    fetchPosts();
+  }, [])
 
 
   return (
@@ -23,7 +33,7 @@ const Feed = () => {
           required className='search_input peer'/>
       </form>
       <PromptCardList 
-        data={[]}
+        data={posts}
         handleTagClick={() => {}}
       />
     </section>
--- components/PromptCardList.tsx
+++ components/PromptCardList.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import PromptCard from './PromptCard';
 
 interface PromptCardListProps {
     data: any;
-    handleTagClick: () => {};
+    handleTagClick: () => void;
 }
 
 const PromptCardList: React.FC<PromptCardListProps> = ({data, handleTagClick}) => {
   return (
     <div className='mt-16 prompt_layout'>
-        
+        {data.map((post: any) => (
+            <PromptCard 
+                key={post.id}
+                post={post}
+                handleTagClick={handleTagClick}
+            />
+        ))}
     </div>
   )
 }
